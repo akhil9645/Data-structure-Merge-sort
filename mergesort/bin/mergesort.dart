b30960046470e8d273a0dd7a9befce42739fcf80
Akhil Jose <124168112+akhil9645@users.noreply.github.com>
2023-05-28 12:42:43 +0530

    Add files via upload

---
+++ mergesort/bin/mergesort.dart
@@ -0,0 +1,36 @@
+List<int> mergesorting(List<int> array) {
+  if (array.length <= 1) {
+    return array;
+  }
+  var mid = array.length / 2.toInt();
+  List<int> firsthalf = array.sublist(0, mid.toInt());
+  List<int> secondhalf = array.sublist(mid.toInt(), array.length);
+  return join(mergesorting(firsthalf), mergesorting(secondhalf));
+}
+
+List<int> join(List<int> firsthalf, List<int> secondhalf) {
+  List<int> newarr = List<int>.filled(firsthalf.length + secondhalf.length, 0);
+  int i = 0;
+  int j = 0;
+  int k = 0;
+  while (i < firsthalf.length && j < secondhalf.length) {
+    if (firsthalf[i] <= secondhalf[j]) {
+      newarr[k++] = firsthalf[i++];
+    } else {
+      newarr[k++] = secondhalf[j++];
+    }
+  }
+  while (i < firsthalf.length) {
+    newarr[k++] = firsthalf[i++];
+  }
+  while (j < secondhalf.length) {
+    newarr[k++] = secondhalf[j++];
+  }
+  return newarr;
+}
+
+void main(List<String> args) {
+  List<int> array = [12, 45, 88, 22, -2, 4, 9];
+  List<int> add = mergesorting(array);
+  print(add);
+}
---
+++ mergesort/test/mergesort_test.dart
@@ -0,0 +1,8 @@
+import 'package:mergesort/mergesort.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
